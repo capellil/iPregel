825e4dccd6709f20886e2927a006838afc6ffaca
Ludovic CAPELLI <capelli.ludovic@gmail.com>
2017-11-27 19:31:44 +0900

    Dump added.

--- benchmarks/sssp.c
+++ benchmarks/sssp.c
@@ -89,27 +89,35 @@ void serialise_vertex(FILE* f, struct vertex_t* v)
 
 int main(int argc, char* argv[])
 {
-	if(argc != 2) 
+	if(argc != 3) 
 	{
 		printf("Incorrect number of parameters.\n");
 		return -1;
 	}
 
-	FILE* f = fopen(argv[1], "rb");
-	if(!f)
+	FILE* f_in = fopen(argv[1], "rb");
+	if(!f_in)
+	{
+		perror("File opening failed.");
+		return -1;
+	}
+	
+	FILE* f_out = fopen(argv[2], "wb");
+	if(!f_out)
 	{
 		perror("File opening failed.");
 		return -1;
 	}
 
 	unsigned int number_of_vertices = 0;
-	if(fread(&number_of_vertices, sizeof(unsigned int), 1, f) != 1)
+	if(fread(&number_of_vertices, sizeof(unsigned int), 1, f_in) != 1)
 	{
 		perror("Could not read the number of vertices.");
 		exit(-1);
 	}
-	init(f, number_of_vertices);
+	init(f_in, number_of_vertices);
 	run();
+	dump(f_out);
 
 	return EXIT_SUCCESS;
 }
